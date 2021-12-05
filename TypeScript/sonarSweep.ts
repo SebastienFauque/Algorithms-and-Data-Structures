f7d83d807e171f45553213ca2d6936888d7c3820
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-12-04 21:37:46 -0800

    solved with a helper function. Pushed some new formatting.

--- TypeScript/sonarSweep.ts
+++ TypeScript/sonarSweep.ts
@@ -1,3 +1,14 @@
+const fs = require('fs');
+const text: string = fs.readFileSync("TypeScript/sonarSweepText.txt", 'utf-8');
+const textByLine: string[] = text.split('\n');
+
+const parse = (testSuite: string[]) => {
+  const inputNums: number[] = [];
+  for (let i: number = 0; i < testSuite.length; i++) {
+    inputNums.push(parseInt(testSuite[i]));
+  }
+  return inputNums;
+}
 /**
  * The advent of code www.adventofcode.com
 *
@@ -82,26 +93,49 @@ const sonarSweep = (depths: number[]) => {
 
   return count;
 }
-
-
 // Solving the example
 const exampleNums: number[] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-console.log(sonarSweep(exampleNums)) // 7
+console.log(sonarSweep(exampleNums)) // 7 Solved!
 
 // Solving the real test
-const fs = require('fs');
-const text: string = fs.readFileSync("TypeScript/sonarSweepText.txt", 'utf-8');
-const textByLine: string[] = text.split('\n');
+console.log(sonarSweep(parse(textByLine))); //1195 Solved!
 
-const parse = (testSuite: string[]) => {
-  const inputNums: number[] = [];
-  for (let i: number = 0; i < testSuite.length; i++) {
-    inputNums.push(parseInt(testSuite[i]));
-  }
-  return inputNums;
-}
 
-// console.log(sonarSweep(parse(textByLine))); //1195 Solved!
+/**
+ *
+ * --- Part Two ---
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A
+200  A B
+208  A B C
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G
+269    F G H
+260      G H
+263        H
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+ */
 
 const sonarSweepSum = (depths: number[]) => {
   if (!depths || !depths.length) return undefined;
@@ -125,4 +159,4 @@ const sonarSweepSum = (depths: number[]) => {
 }
 
 
-console.log(sonarSweepSum(parse(textByLine)));
+console.log(sonarSweepSum(parse(textByLine))); // 1235 Solved!
