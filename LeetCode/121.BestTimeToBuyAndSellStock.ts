803f5a572635efe91ee249133c89a1efc18dfa47
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-12-13 10:08:09 -0800

    Solved in O(n) time.

---
+++ LeetCode/121.BestTimeToBuyAndSellStock.ts
@@ -0,0 +1,13 @@
+var maxProfit = function(prices: number[]): number {
+  let minimumPrice: number = prices[0]; //1
+  let maximumProfit: number = 0; //5
+
+  for (let i = 1; i < prices.length; i++) {
+      let currentPrice: number = prices[i]; // 4
+
+      if (currentPrice < minimumPrice) minimumPrice = currentPrice;
+      if (maximumProfit < currentPrice - minimumPrice) maximumProfit = currentPrice - minimumPrice;
+  }
+
+  return maximumProfit;
+};
